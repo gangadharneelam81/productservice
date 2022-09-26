5d6e4cb54a3b5678b155f622e98ea1474ac45c2c
Gangadhar Neelam <gangadharneelam81@gmail.com>
2022-09-26 16:45:15 +0530

    Loading RDS-MySQL DB details from AWS Credentials

---
+++ src/main/java/com/productservice/config/AppConfig.java
@@ -0,0 +1,93 @@
+package com.productservice.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
+import com.google.gson.Gson;
+import com.productservice.model.AWSSecrets;
+
+@Configuration
+public class AppConfig {
+	
+	/**
+	 * @return
+	 */
+	/**
+	 * @return
+	 */
+	@Value("${cloud.aws.credentials.access-key}")
+	private String accessKey;
+	@Value("${cloud.aws.credentials.secret-key}")
+	private String secretKey;
+	@Value("${cloud.aws.region.static}")
+	private String region;
+	
+	@Bean
+	public DataSource dataSource() {
+		AWSSecrets secret = getSecret();
+		
+		return DataSourceBuilder
+				.create()
+			//	.driverClassName("")
+				.url("jdbc:" + secret.getEngine() + "://" + secret.getHost() + ":" + secret.getPort() + "/testdb")
+				.username(secret.getUsername())
+				.password(secret.getPassword())
+				.build();
+				
+	}
+	// Use this code snippet in your app.
+	
+	// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+	// https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/java-dg-samples.html#prerequisites
+
+	private AWSSecrets getSecret() {
+
+	    String secretName = "rds-mysql-secrets";
+	   // String region =region;
+	    Gson gson = new Gson();
+
+	    // Create a Secrets Manager client
+	    AWSSecretsManager client  = AWSSecretsManagerClientBuilder.standard()
+	                                    .withRegion(region)
+	                                    .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+	                                    .build();
+	    
+	    // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+	    // See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+	    // We rethrow the exception by default.
+	    
+	    String secret, decodedBinarySecret;
+	    GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
+	                    .withSecretId(secretName);
+	    GetSecretValueResult getSecretValueResult = null;
+
+	    try {
+	        getSecretValueResult = client.getSecretValue(getSecretValueRequest);
+	    } catch (Exception e) {
+	        // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+	        // Deal with the exception here, and/or rethrow at your discretion.
+	        throw e;
+	    }
+
+	    // Decrypts secret using the associated KMS key.
+	    // Depending on whether the secret is a string or binary, one of these fields will be populated.
+	    if (getSecretValueResult.getSecretString() != null) {
+	        secret = getSecretValueResult.getSecretString();
+	        return gson.fromJson(secret,AWSSecrets.class);
+	    }
+	    return null;
+	}
+
+}
